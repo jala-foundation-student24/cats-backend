944f8d789857546f4b4d6e9bf244383623e5ce4d
jala-foundation-student24 <student24@jala-foundation.org>
2022-08-27 09:50:37 -0300

    feat: endpoints returning mocked data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/cat')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/list')
+  listCats() {
+    return this.appService.listCats();
+  }
+
+  @Get(':id')
+  getCatById(@Param() params: { id: string }) {
+    const { id } = params;
+    return this.appService.getCatById(id);
+  }
+
+  @Post()
+  createCat(@Body() data: any) {
+    return this.appService.createCat(data);
+  }
+
+  @Patch('/favorite/:id')
+  updateCatFavorite(
+    @Param() params: { id: string },
+    @Body() data: { favorite: boolean },
+  ) {
+    const { id } = params;
+    const { favorite } = data;
+
+    return this.appService.updateCatFavorite({ id, favorite });
+  }
+
+  @Patch('/adopt/:id')
+  updateCatAdopted(
+    @Param() params: { id: string },
+    @Body() data: { adopted: boolean },
+  ) {
+    const { id } = params;
+    const { adopted } = data;
+
+    return this.appService.updateCatAdopted({ id, adopted });
   }
 }
